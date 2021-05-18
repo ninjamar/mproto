0c4b9d610bbcfd33ff4d543110c12a872fa019a8
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-05-17 19:16:13 -0600

    Create __init__.py

---
+++ mproto/__init__.py
@@ -0,0 +1,6 @@
+'''
+Python library for managing custom protocols
+Let Python make requests to urls such as "python://abc"
+https://dev.to/pybash/making-a-custom-protocol-handler-and-uri-scheme-part-1-37mh
+
+'''
