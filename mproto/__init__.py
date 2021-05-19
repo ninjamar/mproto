5cad19a4a3d0c7ac5ce16efe7cf5b38e1fb3d25f
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-05-19 15:13:05 -0600

    Delete mproto directory

--- mproto/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Python library for managing custom protocols
-Let Python make requests to urls such as "python://abc"
-https://dev.to/pybash/making-a-custom-protocol-handler-and-uri-scheme-part-1-37mh
-
-'''
--- mproto/defaults.py
+++
@@ -1,7 +0,0 @@
-import urllib.request as urlreq
-
-class TestProto(urlreq.BaseHandler):
-
-  def python_open(self,req):
-    print(dir(req))
-    return req.get_full_url()
